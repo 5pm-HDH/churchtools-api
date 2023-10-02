225470b1482579bb6b7a3e616e65fc41cab508a6
André Schild <a.schild@aarboard.ch>
2023-10-02 10:49:27 +0200

    Added config request

---
+++ src/Models/Common/Config/ConfigRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace CTApi\Models\Common\Config;
+
+
+use CTApi\CTClient;
+use CTApi\Utils\CTResponseUtil;
+
+class ConfigRequest
+{
+    /**
+     * Request Config of ChurchTools-Instance.
+     * @return array contains keys: <code>build</code>, <code>shortName</code>, <code>siteName</code> and <code>version</code>
+     */
+    public static function getConfig(): array
+    {
+        $client = CTClient::getClient();
+        $response = $client->get('/api/config');
+        return CTResponseUtil::jsonToArray($response);
+    }
+}
