24660614c0b6d0991b0c862660a5e07475e9d8ae
stollr <christian.stoller@mail.de>
2024-01-06 17:57:06 +0100

    Added group type model, request and request builder

---
+++ src/Models/Groups/Group/GroupTypeRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace CTApi\Models\Groups\Group;
+
+
+class GroupTypeRequest
+{
+    public static function all(): array
+    {
+        return (new GroupTypeRequestBuilder())->all();
+    }
+
+    public static function where(string $key, $value): GroupTypeRequestBuilder
+    {
+        return (new GroupTypeRequestBuilder())->where($key, $value);
+    }
+
+    public static function orderBy(string $key, $orderAscending = true): GroupTypeRequestBuilder
+    {
+        return (new GroupTypeRequestBuilder())->orderBy($key, $orderAscending);
+    }
+
+    public static function findOrFail(int $id): GroupType
+    {
+        return (new GroupTypeRequestBuilder())->findOrFail($id);
+    }
+
+    public static function find(int $id): ?GroupType
+    {
+        return (new GroupTypeRequestBuilder())->find($id);
+    }
+
+}
